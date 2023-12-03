9118605f49d163bb9da7bf3e06bb2d976e8a2dea
matryxxx02 <cola992011@hotmail.fr>
2023-12-03 22:59:49 +0100

    try part 2

--- day3/day3.js
+++ day3/day3.js
@@ -38,7 +38,6 @@ function sumOfPartNumber(input) {
      inputVal.forEach((line, indexLine)=> {
         let indexColMatrix = 0;
         const sumPartNumberLine = line.reduce((acc, char, indexChar) => {
-
             if (!isNaN(Number(char)) && isPartNumber(indexLine, indexChar, indexColMatrix, char)) {
                 indexColMatrix += char.length
                 return acc + Number(char)
@@ -53,4 +52,48 @@ function sumOfPartNumber(input) {
 
 }
 
-console.log(sumOfPartNumber(input))
+// console.log(sumOfPartNumber(input))
+
+function sumOfGearRatio(input) {
+    const lines = input.split("\n")
+    const inputVal = lines.map(line => line.match(/(?:\d+|\.|\+|[^0-9+\.])/g))
+    const matrix = lines.map(line => line.split(''))
+    const numberRegex = /\d+/g
+    console.log(inputVal)
+    function gearRatio(indexLine, indexCol, value) {
+        const leftIsNumber = indexCol > 0 && numberRegex.test(inputVal[indexLine][indexCol - 1]) && window;
+        const rightIsNumber = indexCol < inputVal[indexLine].length - 1 && numberRegex.test(inputVal[indexLine][indexCol + 1]);
+
+        if(leftIsNumber)
+
+        const indexColMin = Math.max(indexCol - 1, 0);
+        const indexColMax = Math.min(indexCol + value.length + 1, matrix[indexLine].length);
+
+        const upperLine = indexLine > 0 && matrix[indexLine - 1].slice(indexColMin, indexColMax).some(val => symbolesRegex.test(val));
+        const lowerLine = indexLine < lines.length - 1 && matrix[indexLine + 1].slice(indexColMin, indexColMax).some(val => symbolesRegex.test(val));
+
+
+        return !!(leftSymbol || rightSymbol || upperLine || lowerLine);
+    }
+
+    let sumAllPartNumbers = 0
+
+    inputVal.forEach((line, indexLine)=> {
+        // let indexColMatrix = 0;
+        const sumPartNumberLine = line.reduce((acc, char, indexChar) => {
+            if (char === "*") {
+                gearRatio(indexLine, indexChar, char)
+                // indexColMatrix += char.length
+                return acc + Number(char)
+            }
+            // indexColMatrix += char.length
+            return acc
+        }, 0)
+        sumAllPartNumbers += sumPartNumberLine
+    })
+
+    return sumAllPartNumbers
+
+}
+
+console.log(sumOfGearRatio(inputExample))
