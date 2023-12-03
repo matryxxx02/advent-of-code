677c1ab6eb12b6ddaa80b24d28e16454cd39a7c8
matryxxx02 <cola992011@hotmail.fr>
2023-12-03 12:26:13 +0100

    Day 2️⃣ - part 1️⃣

--- day2/day2.js
+++ day2/day2.js
@@ -4,7 +4,65 @@ console.log('Seconde day')
 
 const input = readInputFile('./input.txt')
 
-function _() {
+const inputExample = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" +
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" +
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" +
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" +
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
 
+function countPossibleGames(input) {
+    const cubesMax = {
+        red: 12,
+        green: 13,
+        blue: 14,
+    }
+
+    const extractNumbers = (value) => {
+        const regex = /\d+/g
+        return value.match(regex)[0]
+    }
+
+    const gameIsPossible = (value) => {
+        const cubesCount = {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+
+        Object.keys(cubesCount).forEach(cubeColor => {
+            const colorRegex = new RegExp(`\\b(\\d+\\s+${cubeColor})\\b`, 'g');
+            const cubeValues = value.match(colorRegex)
+            if(cubeValues) {
+                cubesCount[cubeColor] = cubeValues.reduce((acc, val) => {
+                    const cubeCount = parseInt(val.match(/\d+/)[0]);
+                    return acc + cubeCount;
+                }, 0);
+            }
+        })
+
+        return Object.keys(cubesMax).every((color) => {
+            return cubesCount[color] <= cubesMax[color];
+        });
+    }
+
+    const lines = input.split("\n")
+    return lines.reduce(((acc, line) => {
+        const splitedLine = line.split(':')
+        const gameNumber = Number(extractNumbers(splitedLine[0]))
+        const setGames = splitedLine[1].split(';')
+
+        for(let game of setGames) {
+            const possibleGame = gameIsPossible(game)
+            if (!possibleGame) {
+                console.log(`Set ${gameNumber} ❌`)
+                return acc
+            }
+        }
+
+        console.log(`Set ${gameNumber} ✅ `)
+
+        return acc + gameNumber;
+    }), 0)
 }
 
+console.log(countPossibleGames(input))
