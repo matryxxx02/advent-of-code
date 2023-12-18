572d5e739a5fce38b297c98bff95e8d37449a781
matryxxx02 <cola992011@hotmail.fr>
2023-12-18 23:45:56 +0100

    fuck

--- day18/day18.js
+++ day18/day18.js
@@ -21,7 +21,7 @@ function getDirections(input) {
     const lines = input.split('\n')
     return lines.map(line => {
         const values = line.split(" ")
-        return ({direction: values[0], count: values[1]})
+        return ({direction: values[0], count: Number(values[1])})
     })
 }
 
@@ -30,34 +30,111 @@ function dig(input) {
     const matrix = [['#']]
     let i = 0
     let j = 0
+    let newI
+    let newJ
 
     for (let {direction, count} of directions) {
+        // console.log({i, j, direction, count})
         switch(direction) {
             case 'R' :
-                j += count
-                // matrix[index.x].push(Array.from({ length: count }, () => '#'))
-                if(j <= matrix[i].length)
+                newJ = j + count
+
+                if(newJ >= matrix[i].length) {
                     //ajouter a tous les lignes les char manquant + '#'
-                if
+                    matrix.forEach((line, idx) => {
+                        if(idx === i) matrix[idx].push(...Array.from({ length: count }, () => '#'))
+                        else matrix[idx].push(...Array.from({ length: count }, () => '.'))
+                    })
+                } else {
+                    matrix[i].splice(j, count, ...Array.from({ length: count }, () => '#'))
+                }
+                j = newJ
+
                 break;
             case 'L' :
-                j -= count
-                if(j < 0)
-                    //ajouter a tous les lignes les char manquant + '#'
+                newJ = j - count
 
-                const start = index.x-count
-                // matrix.forEach((line, idx) => {
-                //
-                // })
-                // matrix[index.x].splice(start > 1 ? start : 0, 0, Array.from({ length: count }, () => '#'))
+                if(newJ < 0){
+                    //ajouter a tous les lignes les char manquant + '#'
+                    matrix.forEach((line, idx) => {
+                        if(idx === i) matrix[idx].splice(0,0, ...Array.from({ length: count }, () => '#'))
+                        else matrix[idx].splice(0,0, ...Array.from({ length: count }, () => '.'))
+                    })
+                    j = 0
+                } else {
+                    matrix[i].splice(newJ, count, ...Array.from({ length: count }, () => '#'))
+                    j = newJ
+                }
+
                 break;
             case 'U' :
-                x += count
-                break;
-            case 'D' :
-                x -= count
+                newI = i - count
+                if(newI < 0){
+                    // ajouter des lignes en haut
+                    for(let indice = i; indice>newI ;indice--) {
+                        const newLine = Array.from({ length: matrix[i].length }, () => '.')
+                        newLine[j] = '#'
+                        matrix.splice(0,0,newLine)
+                    }
+                    i = 0
+                } else {
+                    for(let indice = i; indice>newI ;indice--) {
+                        matrix[indice][j] = '#'
+                    }
+                    i = newI
+                }
+
                 break;
 
+            case 'D' :
+                newI = i + count
+                if(newI >= matrix.length) {
+                    // ajouter des lignes en bas
+                    for(let indice = i; indice<newI ;indice++) {
+                        const newLine = Array.from({ length: matrix[i].length }, () => '.')
+                        newLine[j] = '#'
+                        matrix.push(newLine)
+                    }
+                } else {
+                    for(let indice = i; indice<newI ;indice++) {
+                        matrix[indice][j] = '#'
+                    }
+                }
+                i = newI
+
+                break;
         }
     }
+
+    return matrix
 }
+
+function displayMatrix(matrix) {
+    return matrix.reduce((acc, line) => acc+ (line.join('') + '\n'), "")
+}
+
+function digOutTheInterior(array) {
+    let tableauModifie = array;
+    // console.log(array)
+    array.filter(line => line.filter((l) => l === '#').length >= 2).forEach((line, i) => {
+        let inside = false
+        console.log({rr: Object.entries(line)})
+        for(let [j, char] of Object.entries(line)) {
+            console.log({inside, char})
+            if(!inside && char === "#") {
+                inside = true
+            } else if(inside && char !== "#") {
+                tableauModifie[i][j] = "#"
+            } else if(inside) {
+                inside = false
+                break;
+            }
+        }
+    })
+    return tableauModifie;
+}
+
+const res = dig(inputExample)
+console.log(displayMatrix(res))
+const resInter = digOutTheInterior(res)
+console.log(displayMatrix(resInter))
